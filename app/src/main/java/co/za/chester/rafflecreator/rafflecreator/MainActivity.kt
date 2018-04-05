6ce4b7abb6127beda325b0c1e5d8a129e64a4cd7
Chester Cobus <cacobus15@gmail.com>
2018-04-05 23:21:14 +0200

    develop: added popup dialog to add raffle name

--- app/src/main/java/co/za/chester/rafflecreator/rafflecreator/MainActivity.kt
+++ app/src/main/java/co/za/chester/rafflecreator/rafflecreator/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
+import android.support.v7.app.AlertDialog
+import android.widget.EditText
+import android.view.LayoutInflater
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,20 +18,49 @@ class MainActivity : AppCompatActivity() {
         resetExitCounter()
     }
 
-    private fun resetExitCounter(){
+    private fun resetExitCounter() {
         exitCounter = 2
     }
 
     override fun onBackPressed() {
         --exitCounter
-        when (exitCounter){
-            1 -> Toast.makeText(this, "Press back again and app will be exited!", Toast.LENGTH_LONG).show()
+        when (exitCounter) {
+            1 -> Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_LONG).show()
             0 -> this.finish()
         }
     }
 
-    fun addRaffle(view: View){
+    fun addRaffle(view: View) {
         resetExitCounter()
-        Toast.makeText(this, "TODO: Popup to enter Raffle name", Toast.LENGTH_LONG).show()
+        openRaffleNameDialog()
+    }
+
+    private fun openRaffleNameDialog() {
+        val layoutInflater = LayoutInflater.from(this)
+        val promptView = layoutInflater.inflate(R.layout.prompt, null)
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setView(promptView)
+
+        val raffleNameInput = promptView
+                .findViewById(R.id.editTextDialogRaffleNameInput) as EditText
+        alertDialogBuilder
+                .setPositiveButton("Add",
+                        { dialog, _ ->
+                            val raffleName = raffleNameInput.text
+                            if (raffleName.isNullOrEmpty()) {
+                                Toast.makeText(this, "No Raffle Name entered", Toast.LENGTH_LONG).show()
+                                dialog.dismiss()
+                            } else {
+                                Toast.makeText(this, "$raffleName added to list of raffles", Toast.LENGTH_LONG).show()
+                                dialog.dismiss()
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        { dialog, _ ->
+                            dialog.cancel()
+                        })
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
